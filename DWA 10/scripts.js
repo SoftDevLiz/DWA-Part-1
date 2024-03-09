12971ca627847780500fc1034df9a1592282527e
Liezl Mouton <liz.mouton1@gmail.com>
2024-03-09 17:22:17 +0200

    Use sl components + make js work

--- DWA 10/scripts.js
+++ DWA 10/scripts.js
@@ -1,10 +1,11 @@
-const MAX_NUMBER = 10;
-const MIN_NUMBER = -5;
-const STEP_AMOUNT = 5;
+// const MAX_NUMBER = "";
+// const MIN_NUMBER = "";
+const STEP_AMOUNT = 1;
 
 const number = document.querySelector('[data-key="number"]');
 const subtract = document.querySelector('[data-key="subtract"]');
 const add = document.querySelector('[data-key="add"]');
+const reset = document.querySelector('[data-key="reset"');
 
 const subtractHandler = () => {
   const newValue = parseInt(number.value) - STEP_AMOUNT;
@@ -32,5 +33,10 @@ const addHandler = () => {
   }
 };
 
+const resetHandler = () => {
+  number.value = 0;
+}
+
 add.addEventListener("click", addHandler);
 subtract.addEventListener("click", subtractHandler);
+reset.addEventListener("click", resetHandler);
